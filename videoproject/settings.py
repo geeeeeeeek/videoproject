fed91e5576f5de8561897295dc375cba13ad3698
gk2007 <lovezww2011@gmail.com>
2024-05-28 15:48:38 +0800

    Update settings.py

--- videoproject/settings.py
+++ videoproject/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'video',
         'USER': 'root',
-        'PASSWORD': '4643830',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
